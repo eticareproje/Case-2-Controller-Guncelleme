9c4084a8719be1e61f97234d53089867d4f2bdf4
Hasan Yılmaz <yy.hasan@hotmail.com>
2018-11-23 23:57:20 +0300

    Case 2: Controller Guncelleme

---
+++ WebApiCarSharing/Controllers/datetime.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiCarSharing.Controllers
+{
+    public class datetime
+    {
+        public DateTime date()
+        {
+            DateTime date = new DateTime();
+            date = DateTime.Today;
+            return date;
+
+        }
+    }
+    
+}
