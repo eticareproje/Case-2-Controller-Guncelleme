29cca640c09acf5488aa1097c14f2d15d88baa22
Fatih BERBER <fatihberber43@gmail.com>
2018-11-24 00:06:42 +0300

    güncelleme

--- WebApiCarSharing/Controllers/AracController.cs
+++ WebApiCarSharing/Controllers/AracController.cs
@@ -16,7 +16,7 @@ namespace WebApiCarSharing.Controllers
     {
         private RentACarEntities db = new RentACarEntities();
 
-        // GET: api/Aracaa
+        // GET: api/Aracaaf
         public IQueryable<Arac> GetArac()
         {
             return db.Arac;
